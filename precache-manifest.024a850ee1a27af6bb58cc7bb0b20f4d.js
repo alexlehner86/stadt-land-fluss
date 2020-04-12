68c2e85e12c1c0c9e8bb748bfd53c66b92184da7
Alexander Lehner <alexander.lehner@tieto.com>
2020-04-12 22:50:32 +0200

    Updates

--- precache-manifest.024a850ee1a27af6bb58cc7bb0b20f4d.js
+++ precache-manifest.024a850ee1a27af6bb58cc7bb0b20f4d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f84ffce9942012ec3c7e28fe6d29ef3a",
+    "revision": "55c2e319a19a87f037ca37b86b5a8b49",
     "url": "/stadt-land-fluss/index.html"
   },
   {
-    "revision": "20fc2244af28ec158d1d",
-    "url": "/stadt-land-fluss/static/css/main.8d3b3810.chunk.css"
+    "revision": "933604124c7cbc105da1",
+    "url": "/stadt-land-fluss/static/css/main.7106f9e8.chunk.css"
   },
   {
     "revision": "a29e8299a8ec3be8210f",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/stadt-land-fluss/static/js/2.ec6c2347.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "20fc2244af28ec158d1d",
-    "url": "/stadt-land-fluss/static/js/main.b669c134.chunk.js"
+    "revision": "933604124c7cbc105da1",
+    "url": "/stadt-land-fluss/static/js/main.80210e55.chunk.js"
   },
   {
     "revision": "b74dfcfc03f84d34d91b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stadt-land-fluss/precache-manifest.a0b7863a5fe9e68ee6b854ed1a4f3d8c.js"
+  "/stadt-land-fluss/precache-manifest.024a850ee1a27af6bb58cc7bb0b20f4d.js"
 );
 
 self.addEventListener('message', (event) => {
