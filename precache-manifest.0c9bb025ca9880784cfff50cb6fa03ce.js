71bc19dd44f2ab99447bfa4f19e1991167a9e93a
Alexander Lehner <alexander.lehner@tieto.com>
2020-05-03 09:18:01 +0200

    Updates

---
+++ precache-manifest.0c9bb025ca9880784cfff50cb6fa03ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14298016fb8775f5ea4cc4046acf9a75",
+    "url": "/stadt-land-fluss/index.html"
+  },
+  {
+    "revision": "2f3eea37b37ec837718a",
+    "url": "/stadt-land-fluss/static/css/main.ad55a08a.chunk.css"
+  },
+  {
+    "revision": "6cf723eb6085c0d0cc96",
+    "url": "/stadt-land-fluss/static/js/2.7d78bf34.chunk.js"
+  },
+  {
+    "revision": "380b27b1e088253ea38ef66b11c645a9",
+    "url": "/stadt-land-fluss/static/js/2.7d78bf34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3eea37b37ec837718a",
+    "url": "/stadt-land-fluss/static/js/main.db2f19e8.chunk.js"
+  },
+  {
+    "revision": "b74dfcfc03f84d34d91b",
+    "url": "/stadt-land-fluss/static/js/runtime-main.750b6d87.js"
+  }
+]);
